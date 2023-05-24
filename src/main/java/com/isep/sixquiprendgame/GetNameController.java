5c61733f8fbed039c95e48b36257012f7ed32f0d
charlottedvl <charlotte.deville@eleve.isep.fr>
2023-05-24 16:06:39 +0200

    Get Name of the player

--- src/main/java/com/isep/sixquiprendgame/GetNameController.java
+++ src/main/java/com/isep/sixquiprendgame/GetNameController.java
@@ -2,18 +2,22 @@ package com.isep.sixquiprendgame;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
 public class GetNameController {
 
     @FXML
     private Button changeSceneButton;
-
+    @FXML
+    private TextField nameField;
     @FXML
     private VBox view;
     @FXML
     protected void confirmButtonAction() {
         String filename = "GetName.fxml";
+        String name = nameField.getText();
+        HumanPlayer player = new HumanPlayer(name);
         Tool.load(filename, view, changeSceneButton);
     }
 }
